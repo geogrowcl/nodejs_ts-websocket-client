c7e48869074f29a7b137dfbf10ecb100018f30f9
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-06-23 13:28:11 -0400

    feat(main): get env vars

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@
 
 
 function main(): void{
-
+  const os: NodeJS.ProcessEnv = process.env;
+  const url: string | undefined = os['URL'];
+  const port: number = parseInt(`${os['PORT'] || 3000 }`)
+  const userId: string | undefined = os['USER_ID'];
+  
 
   console.log("Hola Mundo");
 
